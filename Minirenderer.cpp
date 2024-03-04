95adeac537188a29f2af6171051c459b395656c7
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-03-04 20:30:11 +0100

    Add project files.

---
+++ Minirenderer.cpp
@@ -0,0 +1,12 @@
+﻿// Minirenderer.cpp : Defines the entry point for the application.
+//
+
+#include "Minirenderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
