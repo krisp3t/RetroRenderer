e554f35dbdfaab6bb0045e9dccd677db2bcf1d88
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2024-10-12 19:13:30 +0200

    Add colors to Logger

--- src/Base/Logger.cpp
+++ src/Base/Logger.cpp
@@ -1,5 +1,5 @@
 /**
- * Simple thread-safe logger for debugging purposes. Only in debug builds.
+ * Simple thread-safe logger for debugging purposes. No-op in release builds.
  */
 
 #include <iostream>
@@ -12,6 +12,13 @@ namespace RetroRenderer
 LogLevel Logger::_minLevel = LogLevel::LOG_INFO;
 std::mutex Logger::_mutex;
 
+// ANSI color codes
+const char* color_reset = "\u001b[0m";
+const char* color_info = "\u001b[37m";     // White
+const char* color_debug = "\u001b[36m";    // Cyan
+const char* color_warn = "\u001b[33m";     // Yellow
+const char* color_error = "\u001b[31m";    // Red
+
 #ifndef NDEBUG
 void Logger::Log(LogLevel level, const char *file, int line, const char *message)
 {
@@ -21,10 +28,31 @@ void Logger::Log(LogLevel level, const char *file, int line, const char *message
     {
         return;
     }
-    std::cout << "[" << timestamp << "] " << message << std::endl;
+    std::cout << "[" << timestamp << "] ";
+    switch (level)
+    {
+    case LogLevel::LOG_INFO:
+        std::cout << color_info << "[INFO] ";
+        break;
+    case LogLevel::LOG_DEBUG:
+        std::cout << color_debug << "[DEBUG] ";
+        break;
+    case LogLevel::LOG_WARN:
+        std::cout << color_warn << "[WARNING] ";
+        break;
+    case LogLevel::LOG_ERROR:
+        std::cout << color_error << "[ERROR] ";
+        break;
+    }
+
+    std::cout << message << color_reset << std::endl;
+
+    if (level > LogLevel::LOG_INFO)
+    {
+        std::cout << file << ":" << line << std::endl;
+    }
 }
 #else
-// no-op on release builds
 void Logger::Log(LogLevel level, const char *file, int line, const char *message) {}
 #endif
 
