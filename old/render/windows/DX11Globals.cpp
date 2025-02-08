8c2bf588ef87cad8348f6da2b14b01cf14cd0d73
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-02-08 19:42:01 +0100

    Update CMakeLists.txt, delete old/

--- old/render/windows/DX11Globals.cpp
+++
@@ -1,9 +0,0 @@
-#include "DX11Globals.h"
-
-namespace KrisRenderer::DX11Globals {
-    Microsoft::WRL::ComPtr< ID3D11Device> sDx11Device = nullptr;
-    Microsoft::WRL::ComPtr<ID3D11DeviceContext> sDx11DeviceContext = nullptr;
-	Microsoft::WRL::ComPtr<IDXGIFactory2> sDxgiFactory = nullptr;
-    Microsoft::WRL::ComPtr<ID3D11Debug> sDx11Debug = nullptr;
-    std::unique_ptr<DX11DebugCleanup> sDx11DebugCleanup = nullptr;
-}
--- old/utility.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <glm/vec2.hpp>
-
-namespace KrisRenderer 
-{
-	uint32_t rgbaToHexArgb(float(&fg_color)[4]);
-	uint32_t rgbaToHexRgba(float(&fg_color)[4]);
-	glm::vec2 NDC_to_Screen(const glm::vec2& ndc, const int winWidth, const int winHeight);
-}
