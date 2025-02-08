bf95ae6bf2609820866bda37a871172836eb1efd
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-02-08 23:04:17 +0100

    Add android-project template

---
+++ android-project/app/build.gradle.kts
@@ -0,0 +1,75 @@
+import com.android.build.api.dsl.Packaging
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.krisp3t.retrorenderer"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.krisp3t.retrorenderer"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.0.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        externalNativeBuild {
+            cmake {
+                arguments += listOf("-DANDROID_APP_PLATFORM=android-21", "-DANDROID_STL=c++_shared")
+                abiFilters += listOf("arm64-v8a")
+            }
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("./src/main/cpp/CMakeLists.txt")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isDebuggable = true
+            isJniDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            packaging {
+                jniLibs.keepDebugSymbols += "**/*.so"
+            }
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        prefab = true
+    }
+    lint {
+        abortOnError = false
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.games.activity)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
