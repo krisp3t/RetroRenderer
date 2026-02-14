ba8085b526bee0544ea9ebc45bde0cc35a9fb303
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2026-02-14 18:42:30 +0100

    Add golden tests

--- tests/GoldenRenderingTests.cpp
+++ tests/GoldenRenderingTests.cpp
@@ -49,9 +49,15 @@ bool ShouldRunGoldensOnCurrentPlatform() {
 #endif
 }
 
-Config MakePipelineConfig(bool depthTest, bool rasterClip) {
+void SkipGoldenOnUnsupportedPlatform() {
+    if (!ShouldRunGoldensOnCurrentPlatform()) {
+        SKIP("Set RETRO_RUN_GOLDEN_WINDOWS=1 to enforce golden hashes on Windows.");
+    }
+}
+
+Config MakePipelineConfig(bool depthTest, bool rasterClip, bool backfaceCulling = false) {
     Config config{};
-    config.cull.backfaceCulling = false;
+    config.cull.backfaceCulling = backfaceCulling;
     config.cull.depthTest = depthTest;
     config.cull.rasterClip = rasterClip;
     config.software.rasterizer.polygonMode = Config::RasterizationPolygonMode::FILL;
@@ -81,7 +87,8 @@ bool RenderScenarioToHash(const std::string& objText,
                           bool depthTest,
                           bool rasterClip,
                           const std::array<Pixel, 4>& faceColors,
-                          uint64_t& outHash) {
+                          uint64_t& outHash,
+                          bool backfaceCulling = false) {
     LightweightObjSceneImporter importer;
     ImportedSceneData sceneData{};
     if (!importer.LoadFromMemory(reinterpret_cast<const uint8_t*>(objText.data()), objText.size(), sceneData)) {
@@ -99,7 +106,7 @@ bool RenderScenarioToHash(const std::string& objText,
     Buffer<float> depthBuffer(64, 64);
     framebuffer.Clear(Pixel{0, 0, 0, 0});
     depthBuffer.Clear(1.0f);
-    const Config config = MakePipelineConfig(depthTest, rasterClip);
+    const Config config = MakePipelineConfig(depthTest, rasterClip, backfaceCulling);
 
     size_t face = 0;
     for (size_t i = 0; i < mesh.indices.size(); i += 3, face++) {
@@ -139,9 +146,7 @@ void AssertGoldenHash(const std::string& key, uint64_t actualHash) {
 } // namespace
 
 TEST_CASE("Golden software pipeline hash: simple triangle", "[golden][software]") {
-    if (!ShouldRunGoldensOnCurrentPlatform()) {
-        SKIP("Set RETRO_RUN_GOLDEN_WINDOWS=1 to enforce golden hashes on Windows.");
-    }
+    SkipGoldenOnUnsupportedPlatform();
     const std::string objText =
         "v -0.7 -0.7 0.0\n"
         "v 0.7 -0.7 0.0\n"
@@ -158,9 +163,7 @@ TEST_CASE("Golden software pipeline hash: simple triangle", "[golden][software]"
 }
 
 TEST_CASE("Golden software pipeline hash: depth overlap", "[golden][software]") {
-    if (!ShouldRunGoldensOnCurrentPlatform()) {
-        SKIP("Set RETRO_RUN_GOLDEN_WINDOWS=1 to enforce golden hashes on Windows.");
-    }
+    SkipGoldenOnUnsupportedPlatform();
     const std::string objText =
         "v -0.6 -0.6 -0.8\n"
         "v 0.6 -0.6 -0.8\n"
@@ -181,9 +184,7 @@ TEST_CASE("Golden software pipeline hash: depth overlap", "[golden][software]") 
 }
 
 TEST_CASE("Golden software pipeline hash: clipping", "[golden][software]") {
-    if (!ShouldRunGoldensOnCurrentPlatform()) {
-        SKIP("Set RETRO_RUN_GOLDEN_WINDOWS=1 to enforce golden hashes on Windows.");
-    }
+    SkipGoldenOnUnsupportedPlatform();
     const std::string objText =
         "v 1.2 -0.2 0.0\n"
         "v 1.6 -0.2 0.0\n"
@@ -201,6 +202,106 @@ TEST_CASE("Golden software pipeline hash: clipping", "[golden][software]") {
                                  {Pixel{255, 120, 0, 255}, Pixel{0, 220, 140, 255}, Pixel{0, 0, 0, 0}, Pixel{0, 0, 0, 0}},
                                  actualHash));
     AssertGoldenHash("integration_clipping", actualHash);
+}
+
+TEST_CASE("Golden software pipeline hash: partial clipping", "[golden][software]") {
+    SkipGoldenOnUnsupportedPlatform();
+    const std::string objText =
+        "v 0.1 -0.7 0.0\n"
+        "v 1.4 -0.7 0.0\n"
+        "v 0.8 0.7 0.0\n"
+        "v -0.9 -0.7 0.0\n"
+        "v -0.1 -0.7 0.0\n"
+        "v -0.5 0.7 0.0\n"
+        "f 1 2 3\n"
+        "f 4 5 6\n";
+
+    uint64_t actualHash = 0;
+    REQUIRE(RenderScenarioToHash(objText,
+                                 false,
+                                 true,
+                                 {Pixel{255, 180, 40, 255}, Pixel{60, 230, 220, 255}, Pixel{0, 0, 0, 0}, Pixel{0, 0, 0, 0}},
+                                 actualHash));
+    AssertGoldenHash("integration_partial_clipping", actualHash);
+}
+
+TEST_CASE("Golden software pipeline hash: backface culling", "[golden][software]") {
+    SkipGoldenOnUnsupportedPlatform();
+    const std::string objText =
+        "v -0.6 -0.5 0.0\n"
+        "v 0.0 0.6 0.0\n"
+        "v 0.6 -0.5 0.0\n"
+        "f 1 2 3\n"
+        "f 1 3 2\n";
+
+    uint64_t actualHash = 0;
+    REQUIRE(RenderScenarioToHash(objText,
+                                 false,
+                                 true,
+                                 {Pixel{250, 210, 60, 255}, Pixel{80, 90, 255, 255}, Pixel{0, 0, 0, 0}, Pixel{0, 0, 0, 0}},
+                                 actualHash,
+                                 true));
+    AssertGoldenHash("integration_backface_culling", actualHash);
+}
+
+TEST_CASE("Golden software pipeline hash: degenerate face ignored", "[golden][software]") {
+    SkipGoldenOnUnsupportedPlatform();
+    const std::string objText =
+        "v -0.8 -0.8 0.0\n"
+        "v 0.0 0.0 0.0\n"
+        "v 0.8 0.8 0.0\n"
+        "v -0.6 -0.6 0.0\n"
+        "v 0.6 -0.6 0.0\n"
+        "v 0.0 0.6 0.0\n"
+        "f 1 2 3\n"
+        "f 4 5 6\n";
+
+    uint64_t actualHash = 0;
+    REQUIRE(RenderScenarioToHash(objText,
+                                 false,
+                                 true,
+                                 {Pixel{255, 80, 20, 255}, Pixel{20, 200, 120, 255}, Pixel{0, 0, 0, 0}, Pixel{0, 0, 0, 0}},
+                                 actualHash));
+    AssertGoldenHash("integration_degenerate_face", actualHash);
+}
+
+TEST_CASE("Golden software pipeline hash: painter order without depth test", "[golden][software]") {
+    SkipGoldenOnUnsupportedPlatform();
+    const std::string objText =
+        "v -0.6 -0.6 -0.8\n"
+        "v 0.6 -0.6 -0.8\n"
+        "v 0.0 0.6 -0.8\n"
+        "v -0.6 -0.6 0.8\n"
+        "v 0.6 -0.6 0.8\n"
+        "v 0.0 0.6 0.8\n"
+        "f 1 2 3\n"
+        "f 4 5 6\n";
+
+    uint64_t actualHash = 0;
+    REQUIRE(RenderScenarioToHash(objText,
+                                 false,
+                                 true,
+                                 {Pixel{255, 40, 40, 255}, Pixel{40, 120, 255, 255}, Pixel{0, 0, 0, 0}, Pixel{0, 0, 0, 0}},
+                                 actualHash));
+    AssertGoldenHash("integration_painter_no_depth", actualHash);
+}
+
+TEST_CASE("Golden software pipeline hash: fan triangulation", "[golden][software]") {
+    SkipGoldenOnUnsupportedPlatform();
+    const std::string objText =
+        "v -0.7 -0.7 0.0\n"
+        "v 0.7 -0.7 0.0\n"
+        "v 0.7 0.7 0.0\n"
+        "v -0.7 0.7 0.0\n"
+        "f 1 2 3 4\n";
+
+    uint64_t actualHash = 0;
+    REQUIRE(RenderScenarioToHash(objText,
+                                 false,
+                                 true,
+                                 {Pixel{250, 70, 70, 255}, Pixel{70, 250, 120, 255}, Pixel{0, 0, 0, 0}, Pixel{0, 0, 0, 0}},
+                                 actualHash));
+    AssertGoldenHash("integration_face_fan", actualHash);
 }
 
 } // namespace RetroRenderer
